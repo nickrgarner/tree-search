79bc5506a54c336e369f331ea23c25044dbd30e8
Nick Garner <nrgarner@ncsu.edu>
2019-10-07 13:33:50 -0400

    Created TreeMain file with main method

---
+++ src/proj2/TreeMain.java
@@ -0,0 +1,17 @@
+package proj2;
+
+import java.util.Scanner;
+
+public class TreeMain {
+	
+	public char[] pretrav;
+	
+	public char[] posttrav;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
