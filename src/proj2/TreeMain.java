1b2b94ac5d547206e3ee3e7156507005e8a6d92c
Nick Garner <nrgarner@ncsu.edu>
2019-10-07 16:31:12 -0400

    Integrated Tree into TreeMain, started buildTree

--- src/proj2/TreeMain.java
+++ src/proj2/TreeMain.java
@@ -30,6 +30,11 @@ public class TreeMain {
 		for (int i = 0; i < numNodes; i++) {
 			posttrav[i] = posttravString.charAt(i);
 		}
+		
+		// Create tree
+		TreeMain treeMain = new TreeMain();
+		Tree tree1 = treeMain.new Tree();
+		tree1.setRoot(tree1.buildTree(numNodes, 0, posttrav.length));
 	}
 	
 	/**
@@ -53,10 +58,158 @@ public class TreeMain {
 			}
 		}
 		return inputString;
-	}	
+	}
+	
+	class Tree {
+		
+		/** Root of tree */
+		private Node root;
+		/** Number of nodes in the tree */
+		private int size;
+		
+		/**
+		 * Null constructor
+		 */
+		public Tree() {
+			this(null);
+//			root = new Node(pretrav[0], null, new Node[pretrav.length]);
+//			size = 1;
+		}
+		
+		/**
+		 * Constructor with given root
+		 */
+		public Tree(Node root) {
+			this.root = root;
+			size = 1;
+		}
+		
+		/**
+		 * Returns true if tree is empty.
+		 * @return True if empty.
+		 */
+		public boolean isEmpty() {
+			if (root == null) {
+				return true;
+			}
+			return false;
+		}
+		
+		/**
+		 * Returns total number of nodes in the tree.
+		 * @return Total number of nodes in the tree.
+		 */
+		public int size() {
+			return size;
+		}
+		
+		/**
+		 * Sets root of this tree to the given node
+		 * @param root Node to set root to
+		 */
+		public void setRoot(Node root) {
+			this.root = root;
+		}
+		
+		/**
+		 * Returns the root of this tree
+		 * @return Root node of this tree
+		 */
+		public Node getRoot() {
+			return this.root;
+		}
+		
+		/**
+		 * Recursively builds the tree from preorder and postorder traversal input
+		 * @param size Number of nodes in subtree to be built
+		 * @param prestart Starting point of preorder traversal in pretrav array
+		 * @param poststart Starting point of postorder traversal in postrav array
+		 */
+		public Node buildTree (int size, int prestart, int poststart) {
+			Node subtreeRoot = new Node(pretrav[prestart], null, new Node[size]);
+			int subtreeSize = posttrav.toString().indexOf(posttrav[size - 1]) - pretrav.toString().indexOf(pretrav[prestart + 1]) + 2;
+			subtreeRoot.child[0] = new Node(pretrav[prestart + 1], subtreeRoot, new Node[subtreeSize]);
+			
+			return null;
+		}
+		
+		private class Node {
+			
+			/** Data contained in the Node */
+			private char data;
+			/** Pointer to parent of this Node */
+			private Node parent;
+			/** Array of pointers to child(ren) of this Node */
+			private Node child[];
+			/** Marker to follow ancestry line for relationships */
+			private boolean mark;
+			
+			/**
+			 * Null constructor
+			 */
+			public Node() {
+				this(' ', null, null);
+			}
+			
+			/**
+			 * Constructs a new node from the given data and parent and child pointers.
+			 * @param data Data to contain in the node.
+			 * @param parent Pointer to node's parent
+			 * @param child Array of pointers to node's child(ren)
+			 */
+			public Node (char data, Node parent, Node child[]) {
+				this.data = data;
+				this.parent = parent;
+				this.child = child;
+				mark = false;
+			}
+			
+			/**
+			 * Returns true if this node contains no data
+			 * @return True if this node contains no data
+			 */
+			public boolean isEmpty() {
+				if (data == ' ') {
+					return true;
+				}
+				return false;
+			}
+			
+			/**
+			 * Returns true if this node is the root of the tree
+			 * @return True if node is root
+			 */
+			public boolean isRoot() {
+				if (parent == null) {
+					return true;
+				}
+				return false;
+			}
+			
+			/**
+			 * Returns true if this node has no children
+			 * @return True if node has no children
+			 */
+			public boolean isLeaf() {
+				if (child == null) {
+					return true;
+				}
+				return false;
+			}
+			
+			/**
+			 * Returns true if this node has both parent and child
+			 * @return True if node has both a parent and child
+			 */
+			public boolean isInternal() {
+				if (parent != null && child != null) {
+					return true;
+				}
+				return false;
+			}
+		}
+	}
 }
-
-
 
 
 
