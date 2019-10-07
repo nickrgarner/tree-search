5b12472802f30fa390e8e4d7bd4a0c3670b78064
Nick Garner <nrgarner@ncsu.edu>
2019-10-07 13:57:15 -0400

    Added functionality to fill pretrav and posttrav arrays in Main

--- src/proj2/TreeMain.java
+++ src/proj2/TreeMain.java
@@ -4,14 +4,62 @@ import java.util.Scanner;
 
 public class TreeMain {
 	
-	public char[] pretrav;
+	/** Array containing the Pretraversal order from input */
+	public static char pretrav[];
 	
-	public char[] posttrav;
+	/** Array containing the Posttraversal order from input */
+	public static char posttrav[];
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Setup Scanner
+		Scanner input = new Scanner(System.in);
+		String pretravString = getInputString(input);
+		String posttravString = getInputString(input);
+		int numNodes = pretravString.length();
 		
-
+		// Initialize arrays with proper length
+		pretrav = new char[numNodes];
+		posttrav = new char[numNodes];
+		
+		// Fill Pretraversal array
+		for (int i = 0; i < numNodes; i++) {
+			pretrav[i] = pretravString.charAt(i);
+		}
+		
+		// Fill Posttraversal array
+		for (int i = 0; i < numNodes; i++) {
+			posttrav[i] = posttravString.charAt(i);
+		}
 	}
-
+	
+	/**
+	 * Scans the first line of input and returns the number of nodes contained
+	 * @param input Scanner object parsing the tree input
+	 * @return Number of nodes contained in the input
+	 */
+	public static String getInputString (Scanner input) {
+		String inputString = "";
+		String temp = input.next();
+		if (temp.equals("\n")) {
+			temp = input.next();
+		}
+		if (temp.equals(">") || temp.equals("<")) {
+			temp = input.next();
+			while (!temp.equals(".")) {
+				if (!temp.equals(",")) {
+					inputString += temp;
+				}
+				temp = input.next();
+			}
+		}
+		return inputString;
+	}	
 }
+
+
+
+
+
+
+
+
